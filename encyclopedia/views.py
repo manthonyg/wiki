583a58f57a9366c10be99af604fb0c07ff924abd
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-08-20 18:27:05 -0700

    view for individual entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wiki(request, entry):
+
+    valid_entry = util.get_entry(entry)
+
+    if valid_entry:
+        markdowner = Markdown()
+        html_entry = markdowner.convert(valid_entry)
+        title = entry.capitalize()
+
+        return render(request, "encyclopedia/wiki.html", {
+            "entry": html_entry,
+            "title": title
+        })
+    else:
+        error_title = 'Entry not found'
+        error_body = 'No entry matches your request'
+        return render(request, "encyclopedia/error.html", {
+            "error_title": error_title,
+            "error_body": error_body
+        })
